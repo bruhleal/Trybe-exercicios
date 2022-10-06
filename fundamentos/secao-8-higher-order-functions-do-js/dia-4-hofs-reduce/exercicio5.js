d5eeeff8db7637b08210f4d51b1a51ab8868066b
bruhleal <bruna11leal@live.com>
2022-10-06 18:02:30 -0400

    exercicios do dia

---
+++ fundamentos/secao-8-higher-order-functions-do-js/dia-4-hofs-reduce/exercicio5.js
@@ -0,0 +1,11 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  const expectedResult = 20;
+  
+function containsA() {
+   const letter = names.reduce((acc, crr) => )
+}
